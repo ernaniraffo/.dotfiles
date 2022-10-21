f4eed7e85798eed30c60b0b77f8ba66666d35a38
Ernani Raffo <eraffo@ucsc.edu>
2022-10-20 18:16:54 -0700

    made ubuntu install better

--- install.sh
+++ install.sh
@@ -1,5 +1,20 @@
 #/bin/bash
 
+function ubuntu_nvim() {
+    # install neovim
+    sudo apt install neovim
+    sudo apt install python3-neovim
+    sudo apt-get install software-properties-common
+    sudo add-apt-repository ppa:neovim-ppa/stable
+    sudo apt-get update
+    # install again just in case
+    sudo apt-get install neovim
+    # vim-plug install needs curl
+    sudo apt install curl
+    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+}
+
 function stow_packages() {
     stow nvim
     stow git
@@ -16,14 +31,17 @@ function install_on_macOS() {
     stow_packages
     stow zsh
     nvim_plugins
+    brew install llvm
 }
 
 function install_on_ubuntu() {
-    apt-get install neovim
-    apt-get install git
-    apt-get install stow
+    ubuntu_nvim
+    sudo apt install git
+    sudo apt install stow
     stow_packages
     nvim_plugins
+    # install clangd language server
+    sudo apt-get install clangd-12
 }
 
 function usage() {
